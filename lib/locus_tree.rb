57f7871b230f8a65bf4fc4f33fe29fd599771c82
Jan Aerts <jan.aerts@gmail.com>
2009-04-06 16:13:30 +0100

    Changed name from RecTree to LocusTree

--- lib/locus_tree.rb
+++ lib/locus_tree.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'enumerator'
 
 class Range
   def overlaps?(other_range)
@@ -10,7 +11,7 @@ class Range
   end
 end
 
-class RecTree
+class LocusTree
   attr_accessor :root, :min_children, :max_children
   attr_accessor :nodes
   attr_accessor :positive_nodes
@@ -25,7 +26,7 @@ class RecTree
     #create all leaf nodes and first index nodes
     File.open(filename).sort_by{|v| v.to_i}.each do |line|
       position = line.chomp.to_i
-      leaf_node = RecTree::Node.new(self, Range.new(position, position), :leaf)
+      leaf_node = LocusTree::Node.new(self, Range.new(position, position), :leaf)
       leaf_node.level = 0
       self.nodes[0].push(leaf_node)
     end
@@ -36,7 +37,7 @@ class RecTree
       self.nodes[this_level].sort_by{|n| n.range.begin}.each_slice(@max_children) do |node_group|
         min_pos = node_group.collect{|n| n.range.begin}.min
         max_pos = node_group.collect{|n| n.range.end}.max
-        new_node = RecTree::Node.new(self, Range.new(min_pos, max_pos), :index)
+        new_node = LocusTree::Node.new(self, Range.new(min_pos, max_pos), :index)
         new_node.level = this_level + 1
         new_node.children = node_group.to_a
         new_level_members.push(new_node)
@@ -81,19 +82,19 @@ class RecTree
     end
   end
 
-  def search(range, start_node = RecTree.root)
-    if start_node == RecTree.root
-      RecTree.positive_nodes = Array.new
+  def search(range, start_node = LocusTree.root)
+    if start_node == LocusTree.root
+      LocusTree.positive_nodes = Array.new
     end
 
     start_node.children.each do |child|
       if child.overlaps?(range)
         if child.type == :index or child.type == :root
           child.children.each do |grandchild|
-            RecTree.search(range, grandchild)
+            LocusTree.search(range, grandchild)
           end
         else
-          RecTree.positive_nodes.push(child)
+          LocusTree.positive_nodes.push(child)
         end
       end
     end
@@ -101,20 +102,20 @@ class RecTree
 end
 
 if __FILE__ == $0
-  rtree = RecTree.new(2,10)
+  locus_tree = LocusTree.new(2,10)
 
   #Build from the top
-  root = RecTree::Node.new(rtree, Range.new(1,100), :root, nil)
-  child1 = RecTree::Node.new(rtree, Range.new(1,70), :index, root)
-  child2 = RecTree::Node.new(rtree, Range.new(71,100), :index, root)
-  child3 = RecTree::Node.new(rtree, Range.new(35,43), :index, child1)
+  root = LocusTree::Node.new(locus_tree, Range.new(1,100), :root, nil)
+  child1 = LocusTree::Node.new(locus_tree, Range.new(1,70), :index, root)
+  child2 = LocusTree::Node.new(locus_tree, Range.new(71,100), :index, root)
+  child3 = LocusTree::Node.new(locus_tree, Range.new(35,43), :index, child1)
   puts root.children.collect{|c| c.range.to_s}.join("\t")
   puts child3.range.to_s + "\t" + child3.parent.range.to_s
-  puts rtree.root.range.to_s
-  puts rtree.min_children.to_s + "\t" + rtree.max_children.to_s
+  puts locus_tree.root.range.to_s
+  puts locus_tree.min_children.to_s + "\t" + locus_tree.max_children.to_s
 
-  #Build from the bottom (using packed method from http://donar.umiacs.umd.edu/quadtree/docs/rtree_split_rules.html#packed)
-  rectree = RecTree.new(2, 10)
-  rectree.bulk_load(File.dirname(__FILE__) + '/../test/data/test_data.txt')
+  #Build from the bottom (using packed method from http://donar.umiacs.umd.edu/quadtree/docs/locus_tree_split_rules.html#packed)
+  rectree = LocusTree.new(2, 10)
+  rectree.bulk_load(File.dirname(__FILE__) + '/../test/data/bulk_load.tsv')
   puts rectree.nodes.to_yaml
 end
--- test/test_build.rb
+++ test/test_build.rb
@@ -1,22 +1,22 @@
 require 'test/unit'
 require 'enumerator'
 
-require '../lib/rectree.rb'
+require '../lib/locus_tree.rb'
 
 class BulkLoad < Test::Unit::TestCase
   def setup
-    @rtree = RecTree.new(2,3)
-    @rtree.bulk_load(File.dirname(__FILE__) + '/data/bulk_load.tsv')
+    @locus_tree = LocusTree.new(2,3)
+    @locus_tree.bulk_load(File.dirname(__FILE__) + '/data/bulk_load.tsv')
   end
 
   def test_levels
-    assert_equal(20, @rtree.nodes[0].length)
-    assert_equal(7, @rtree.nodes[1].length)
-    assert_equal(3, @rtree.nodes[2].length)
-    assert_equal(1, @rtree.nodes[3].length)
+    assert_equal(20, @locus_tree.nodes[0].length)
+    assert_equal(7, @locus_tree.nodes[1].length)
+    assert_equal(3, @locus_tree.nodes[2].length)
+    assert_equal(1, @locus_tree.nodes[3].length)
   end
 
   def test_depth
-    assert_equal(4, @rtree.depth)
+    assert_equal(4, @locus_tree.depth)
   end
 end
