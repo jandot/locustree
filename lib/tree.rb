630d4f87623821b874e559b489e5f3416804da3d
Jan Aerts <jan.aerts@gmail.com>
2009-07-20 22:07:29 +0100

    Added Tree#top_level

--- lib/tree.rb
+++ lib/tree.rb
@@ -14,59 +14,63 @@ module LocusTree
     belongs_to :container
     has n, :levels
 
-    attr_accessor :positive_nodes
-
-    # == Description
-    #
-    # Returns the root node.
-    #
-    # == Usage
-    #
-    #   tree.root
-    #
-    # ---
-    # *Arguments*:: none
-    # *Returns*:: Node object
-    def root
-      return Node.first(:id => self.root_id)
+    def top_level
+      return self.levels.select{|l| l.number == 0}[0]
     end
 
-    # == Description
-    #
-    # Searches to tree for a locus.
-    #
-    # == Usage
-    #
-    #   tree.search(Locus.new('1', 1, 5000), 1)
-    #
-    # ---
-    # *Arguments*:
-    # * _locus_ (required):: locus to search for. E.g. Locus.new('1', 1, 5000)
-    # * _search_level_ (optional):: level to collect nodes from (default = 0)
-    # * _start_node_ (optional):: node to start search from (default = root node)
-    # *Returns*:: Array of Node objects
-    def search(locus, search_level = 0, start_node = self.root)
-      if start_node == self.root
-        @positive_nodes = Array.new
-      end
+#    # == Description
+#    #
+#    # Returns the root node.
+#    #
+#    # == Usage
+#    #
+#    #   tree.root
+#    #
+#    # ---
+#    # *Arguments*:: none
+#    # *Returns*:: Node object
+#    def root
+#      return Node.first(:id => self.root_id)
+#    end
 
-      if search_level == self.depth
-        @positive_nodes = [self.root]
-        return @positive_nodes
-      end
+#    # == Description
+#    #
+#    # Searches to tree for a locus.
+#    #
+#    # == Usage
+#    #
+#    #   tree.search(Locus.new('1', 1, 5000), 1)
+#    #
+#    # ---
+#    # *Arguments*:
+#    # * _locus_ (required):: locus to search for. E.g. Locus.new('1', 1, 5000)
+#    # * _search_level_ (optional):: level to collect nodes from (default = 0)
+#    # * _start_node_ (optional):: node to start search from (default = root node)
+#    # *Returns*:: Array of Node objects
+#    def search(locus, search_level = 0, start_node = self.root)
+#      if start_node == self.root
+#        @positive_nodes = Array.new
+#      end
+#
+#      if search_level == self.depth
+#        @positive_nodes = [self.root]
+#        return @positive_nodes
+#      end
+#
+#      start_node.children.each do |child_node|
+#        if child_node.locus.overlaps?(locus)
+#          if child_node.level.number > search_level
+#            self.search(locus, search_level, child_node)
+#          else
+#            @positive_nodes.push(child_node)
+#          end
+#        else
+#        end
+#      end
+#      return @positive_nodes
+#    end
 
-      start_node.children.each do |child_node|
-        if child_node.locus.overlaps?(locus)
-          if child_node.level.number > search_level
-            self.search(locus, search_level, child_node)
-          else
-            @positive_nodes.push(child_node)
-          end
-        else
-        end
-      end
-      return @positive_nodes
-    end
+
 
     # == Description
     #
