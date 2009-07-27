d97ad6c75aee77d378b17d6b9df120117deeb255
Jan Aerts <jan.aerts@gmail.com>
2009-07-27 12:53:45 +0100

    Changed testing code in main lib

--- lib/locus_tree.rb
+++ lib/locus_tree.rb
@@ -9,40 +9,45 @@ require File.dirname(__FILE__) + '/container.rb'
 require File.dirname(__FILE__) + '/tree.rb'
 require File.dirname(__FILE__) + '/level.rb'
 require File.dirname(__FILE__) + '/node.rb'
+require File.dirname(__FILE__) + '/feature.rb'
 
-CHROMOSOME_LENGTHS = {'1' => 105,
-                      '2' => 90
+CHROMOSOME_LENGTHS = {'1' => 247249719,
+                      '2' => 242951149,
+                      '3' => 199501827,
+                      '4' => 191273063,
+                      '5' => 180857866,
+                      '6' => 170899992,
+                      '7' => 158821424,
+                      '8' => 146274826,
+                      '9' => 140273252,
+                      '10' => 135374737,
+                      '11' => 134452384,
+                      '12' => 132349534,
+                      '13' => 114142980,
+                      '14' => 106368585,
+                      '15' => 100338915,
+                      '16' => 88827254,
+                      '17' => 78774742,
+                      '18' => 76117153,
+                      '19' => 63811651,
+                      '20' => 62435964,
+                      '21' => 46944323,
+                      '22' => 49691432,
+                      '23' => 154913754,
+                      '24' => 57772954
                      }
 
-#CHROMOSOME_LENGTHS = {'1' => 247249719,
-#                      '2' => 242951149,
-#                      '3' => 199501827,
-#                      '4' => 191273063,
-#                      '5' => 180857866,
-#                      '6' => 170899992,
-#                      '7' => 158821424,
-#                      '8' => 146274826,
-#                      '9' => 140273252,
-#                      '10' => 135374737,
-#                      '11' => 134452384,
-#                      '12' => 132349534,
-#                      '13' => 114142980,
-#                      '14' => 106368585,
-#                      '15' => 100338915,
-#                      '16' => 88827254,
-#                      '17' => 78774742,
-#                      '18' => 76117153,
-#                      '19' => 63811651,
-#                      '20' => 62435964,
-#                      '21' => 46944323,
-#                      '22' => 49691432,
-#                      '23' => 154913754,
-#                      '24' => 57772954
-#                     }
-
 if __FILE__ == $0
-  @container = LocusTree::Container.new(5)
-  puts @container.to_s
-#  @container.bulk_load('../test/data/loci_with_values.gff')
-#  puts @container.to_s
+#  @container = LocusTree::Container.new(1000)
+
+  @container = LocusTree::Container.load_structure('locus_tree.sqlite3')
+
+  positive_nodes = @container.query('24', 500, 50_000, 1)
+  positive_nodes.each do |node|
+    puts node.to_s
+  end
+
+  puts @container.query_single_bin('24', 500, 50_000).to_s
+  puts @container.query_single_bin('24', 49_050, 49_500).to_s
+  puts @container.query_single_bin('24', 999999, 1000002).to_s
 end
